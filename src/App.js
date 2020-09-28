1b80c002e637a58010fe9c7a4466978481b63ff8
Jake Meyers <jakejake92@yahoo.com>
2020-09-28 15:45:33 -0600

    made dropdown component file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
-    {
-        title: "What is React?",
-        content: "React is a front-end Javascript Library"
-    },
-    {
-        title: "Why use React?",
-        content: "React is a favorite JS library among engineers."
-    },
-    {
-        title: "How do you use React?",
-        content: "You use react by creating components"
-    }
-]
+	{
+		title: "What is React?",
+		content: "React is a front-end Javascript Library",
+	},
+	{
+		title: "Why use React?",
+		content: "React is a favorite JS library among engineers.",
+	},
+	{
+		title: "How do you use React?",
+		content: "You use react by creating components",
+	},
+];
 
 export default () => {
-    return (
-        // <Accordion items={ items } />
-        <Search />
-    )
-    
-
+	return (
+		// <Accordion items={ items } />
+		// <Search />
+		<Dropdown />
+	);
 };
